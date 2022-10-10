451195020640b70d8c448b21e55e149dbead839a
ExyKnox <hskhsk22000@gmail.com>
2022-10-10 17:20:04 +0900

    caesarDecrypter class def.

--- caesar-code/caesarDecrypter.h
+++ caesar-code/caesarDecrypter.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <string>
+
 class caesarDecrypter
 {
+	// cipher text를 저장하는 strToDecode.
+	std::string strToDecode;
+	// decrypt 된 평문을 저장하는 decodedStr.
+	std::string decodedStr;
+
+	public:
+		caesarDecrypter(std::string str);
+		void decrypt(int shiftIdx);
+		std::string showDecrypted();
 };
 
